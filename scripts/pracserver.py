f9baf08d2c6d2153ddec5e27e25433a47aeb51e1
Daniel Nyga <nyga@cs.uni-bremen.de>
2018-01-31 00:15:03 +0100

    added grounding module

--- scripts/pracserver.py
+++ scripts/pracserver.py
@@ -2,43 +2,91 @@
 import json
 import os
 import traceback
+from pprint import pprint
 
+from collections import defaultdict
+from dnutils import out, logs
 from rosprac.srv import *
 import rospy
+from std_msgs.msg import String
+
+import prac
+from tools import RStorage
 
 try:
     from prac.core import locations
-    from prac.core.base import PRAC
+    from prac.core.base import PRAC, PRACDatabase
     from prac.core.inference import PRACInference
 except ImportError:
     print 'PRAC was not found. Please make sure it is in your $PYTHONPATH.'
 
-verbose = 2
+verbose = 1
 DEFAULT_CONFIG = os.path.join(locations.user_data, '.pracconf')
 
-prac = PRAC()
-prac.verbose = verbose
+
+wmlogger = logs.getlogger('/pracserver/worldmodel')
 
 
-def prac_inference(req):
-    infer = PRACInference(prac, req.instructions)
-    try:
-        infer.run()
-    except:
-        traceback.print_exc()
-    tasks = []
-    actioncores = []
-    for node in infer.steps():
-        actioncores.append({"actioncore": node.frame.actioncore,
-                            "actionroles": [{k: v.type} for k, v in node.frame.actionroles.items()]})
-    return InstructionsResponse(json.dumps(actioncores))
+class PRACServer:
+
+    def __init__(self):
+        self.prac = prac.PRAC()
+        self.prac.verbose = verbose
+        self.worldmodel = None
+        rospy.init_node('pracserver')
+        rospy.Subscriber("/world_model", String, self.update_worldmodel)
+
+    def prac_inference(self, req):
+        infer = PRACInference(self.prac, req.instructions)
+        try:
+            infer.run()
+            # when done, load the grounding module to ground the concepts
+            ground = self.prac.module('grounding')
+            objdb = PRACDatabase(self.prac)
+            for obj in self.worldmodel.objs:
+                objdb << 'instance_of(%s,%s)' % (obj.name, obj.type)
+            objdb.write()
+            actioncores = []
+            for node in infer.steps():
+                actionroles = defaultdict(list)
+                actioncores.append({'actioncore': node.frame.actioncore, 'actionroles': actionroles})
+                for role, concept in ground(node, objdb, {}):
+                    actionroles[role].append(concept)
+                out('grounding finished')
+        except:
+            traceback.print_exc()
+        return InstructionsResponse(json.dumps(actioncores))
+
+    def serve_forever(self):
+        s = rospy.Service('pracquery', Instructions, self.prac_inference)
+        print "Waiting for instructions to interpret..."
+        rospy.spin()
+
+    def update_worldmodel(self, data):
+        # self.worldmodel = RStorage(json.loads(data.data))
+        self.worldmodel = RStorage({'objs': [
+            {'name': 'o1', 'type': 'banana.n.01'},
+            {'name': 'w2', 'type': 'blender.n.01'},
+            {'name': 'o3', 'type': 'push_button.n.01'},
+            {'name': 'w3', 'type': 'strawberry.n.01'},
+            {'name': 'w4', 'type': 'apple.n.01'},
+            {'name': 'w5', 'type': 'cup.n.01'},
+            {'name': 'w6', 'type': 'chair.n.01'},
+            {'name': 'w7', 'type': 'milk.n.01'},
+            {'name': 'w8', 'type': 'blueberry.n.02'},
+            {'name': 'w9', 'type': 'tomato.n.01'}
+        ]})
+        wmlogger.debug('---------------\nupdated world model:')
+        for obj in self.worldmodel.objs:
+            wmlogger.debug(obj.name, 'is of type', obj.type)
+        wmlogger.debug('---------------')
 
 
-def run_pracserver():
-    rospy.init_node('pracserver')
-    s = rospy.Service('pracquery', Instructions, prac_inference)
-    print "Waiting for instructions to interpret..."
-    rospy.spin()
+logs.loggers({
+    '/pracserver/worldmodel': logs.newlogger(logs.console, level=logs.INFO),
+    '/prac/grounding': logs.newlogger(logs.console, level=logs.DEBUG),
+})
 
 if __name__ == "__main__":
-    run_pracserver()
+    server = PRACServer()
+    server.serve_forever()
---
+++ scripts/tools.py
@@ -0,0 +1,49 @@
+class RStorage(dict, object):
+    '''
+    Recursive extension of web.util.Storage that applies the Storage constructor
+    recursively to all value elements that are dicts.
+    '''
+    __slots__ = ['_utf8']
+
+    def __init__(self, d=None, utf8=False):
+        self._utf8 = utf8
+        if d is not None:
+            for k, v in d.iteritems(): self[k] = v
+
+    def __setattr__(self, key, value):
+        if key in type(self).__slots__:
+            dict.__setattr__(self, key, value)
+        else:
+            self[key] = value
+
+    def __setitem__(self, key, value):
+        if self._utf8 and isinstance(key, str):
+            key = key.encode('utf8')
+        dict.__setitem__(self, key, rstorify(value, utf8=self._utf8))
+
+    def __getattr__(self, key):
+        if key in type(self).__slots__:
+            return dict.__getattr__(self, key)
+        else:
+            try:
+                return self[key]
+            except KeyError:
+                raise AttributeError('%s object has no attribute %s' % (type(self).__name__, key))
+
+    def __delattr__(self, key):
+        try:
+            del self[key]
+        except KeyError as k:
+            raise (AttributeError, key)
+
+    def __repr__(self):
+        return ('<%s ' % type(self).__name__) + dict.__repr__(self) + '>'
+
+
+def rstorify(e, utf8=True):
+    if type(e) is dict:
+        return RStorage(d=e, utf8=utf8)
+    elif type(e) in (list, tuple):
+        return [rstorify(i, utf8=utf8) for i in e]
+    else:
+        return e
